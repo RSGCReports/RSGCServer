26b489873d962bdffb472cb340ad644460ef271f
stefanaz2 <sfrunza@seneca.ca>
2023-04-04 15:31:23 -0400

    Get all and get one cases kind of work somewhat using this code

--- src/routes/api/getReports.js
+++ src/routes/api/getReports.js
@@ -1,9 +1,18 @@
-const { getReportsByUsername } = require('../../../prisma/prismaFunction');
+const {
+  getReportsByUsername,
+  getVehicleBylicensePlateNo,
+} = require('../../../prisma/prismaFunction');
 const logger = require('../../logger');
 
 module.exports = async (req, res) => {
   try {
     const reports = await getReportsByUsername(req.user);
+
+    for (const report of reports) {
+      const vehicle = await getVehicleBylicensePlateNo(report.VehicleInfo[0].licensePlateNo);
+      // console.log(vehicle);
+      report.vehicle = vehicle;
+    }
 
     res.status(200).json({
       status: 'ok',
